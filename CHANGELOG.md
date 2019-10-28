a27e1a902e5a059bec15939f5db2cd26d5610d95
Gilles <gilles.bourgeat@gmail.com>
2019-10-28 21:02:20 +0100

    Bump version 2.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+2.1.0
+- public MIT version, Open Source <3
+
+1.0.6
+- fix compatibility with Thelia 2.4
+
+1.0.5
+- fix query missing is_promo
+
+1.0.4
+- Optimisations
+
+1.0.3
+- Optimisations
+
+1.0.2
+- Fix bug quantity
+
+1.0.1
+- Fix bug quantity multiply by categories attached to the product
---
+++ EasyProductManager.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace EasyProductManager;
+
+use Thelia\Module\BaseModule;
+
+/**
+ * Class EasyProductManager
+ * @package EasyProductManager
+ * @author
+ */
+class EasyProductManager extends BaseModule
+{
+    /** @var string */
+    const DOMAIN_NAME = 'easyproductmanager';
+    const MODULE_NAME = 'EasyProductManager';
+}
